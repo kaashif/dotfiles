56a58810035291f0984d0562edb02e7d5254c798
Kaashif Hymabaccus <kaashif@kaashif.co.uk>
2014-12-15 11:49:18 +0000

    1280 -> 1600

--- scripts/bin/dmenu_normal.sh
+++ scripts/bin/dmenu_normal.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 export PATH=$PATH:$HOME/bin:$HOME/.cabal/bin:$HOME/.racket/6.1/bin
-dmenu_run -w 1280 -p "Run:" -fn 'Terminus:size=8' -nf '#999999' -nb '#101010' -sb '#101010' -sf '#666666'
+dmenu_run -w 1600 -p "Run:" -fn 'Terminus:size=8' -nf '#999999' -nb '#101010' -sb '#101010' -sf '#666666'
--- scripts/bin/dmenu_playlist.sh
+++ scripts/bin/dmenu_playlist.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 
-PLAYLIST=`mpc lsplaylists | dmenu -w 1280 -nb '#101010' -nf '#999999' -sf '#666666' -sb '#101010' -fn 'Terminus:size=8' -p "Play:" | cut -d')' -f1`
+PLAYLIST=`mpc lsplaylists | dmenu -w 1600 -nb '#101010' -nf '#999999' -sf '#666666' -sb '#101010' -fn 'Terminus:size=8' -p "Play:" | cut -d')' -f1`
 if [[ -z "$PLAYLIST" ]]; then
     exit 0
 else
