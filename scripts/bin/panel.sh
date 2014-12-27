988770f0f5c467b25bc53e9a1ba04df0ebeb0c82
Kaashif Hymabaccus <kaashif@kaashif.co.uk>
2014-12-27 12:03:31 +0000

    re-added wifi, volume

--- scripts/bin/panel.sh
+++ scripts/bin/panel.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env zsh
-(while true; do sleep 1 && /home/kaashif/.cabal/bin/hinfo -iF "music volume load time" | cat; done) | dzen2 -dock -h "16" -x "0" -y "0" -w "1600" -bg "#101010" -ta r -fn 'xft:Terminus:size=9' &
+(while true; do sleep 1 && /home/kaashif/.cabal/bin/hinfo -iF "music volume temp wifi load time" | cat; done) | dzen2 -dock -h "16" -x "0" -y "0" -w "1280" -bg "#101010" -ta r -fn '-*-terminus-medium-r-*--*-93' &
 
