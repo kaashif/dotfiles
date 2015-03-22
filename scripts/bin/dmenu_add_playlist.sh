9fc97626c02278daba2f42ed881a384d27fca3d9
Kaashif Hymabaccus <kaashif@kaashif.co.uk>
2015-03-22 18:13:00 +0000

    add to playlist script

---
+++ scripts/bin/dmenu_add_playlist.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env zsh
+
+PLAYLIST=`mpc lsplaylists | dmenu -w 1280 -nb '#101010' -nf '#999999' -sf '#666666' -sb '#101010' -fn 'Terminus:size=8' -p "Play:" | cut -d')' -f1`
+if [[ -z "$PLAYLIST" ]]; then
+    exit 0
+else
+    mpc load "$PLAYLIST" && mpc play
+fi
