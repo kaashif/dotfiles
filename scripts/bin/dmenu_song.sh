db19b20213888101562c8227ef04b49db558fab4
Kaashif Hymabaccus <kaashif@kaashif.co.uk>
2015-03-23 18:53:22 +0000

    1280 -> 1920

--- scripts/bin/dmenu_song.sh
+++ scripts/bin/dmenu_song.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 
-song=`mpc list title | dmenu -w 1280 -nb '#101010' -nf '#999999' -sf '#666666' -sb '#101010' -fn 'Terminus:size=8' -p "Play:"`
+song=`mpc list title | dmenu -w 1680 -nb '#101010' -nf '#999999' -sf '#666666' -sb '#101010' -fn 'Terminus:size=8' -p "Play:"`
 if [[ -z "$song" ]]; then
 	exit 0
 else
