b809cbcbcc70750ea1c82e9952e698aab98081e6
kaashif <kaashif@kaashif.co.uk>
2016-05-14 02:01:03 +0000

    erc update

--- emacs/.ercrc.el
+++ emacs/.ercrc.el
@@ -2,8 +2,14 @@
 (setq erc-server "irc.freenode.net"
 	  erc-port 6667
 	  erc-prompt-for-password nil
-	  erc-autojoin-channels-alist '(("irc.freenode.net" "#ayylmao1234"))
+	  erc-autojoin-channels-alist '(("freenode.net" "#ayylmao1234")
+									("nixers.net" "#unix"))
 	  erc-nick "kaashif1"
 	  erc-user-full-name "Kaashif Hymabaccus"
 	  erc-insert-timestamp-function 'erc-insert-timestamp-left
-	  erc-nickserv-passwords '(("irc.nixers.net" (("kaashif1" . "megadeth")))))
+	  erc-nickserv-passwords '(("irc.nixers.net" (("kaashif1" . "megadeth"))))
+	  erc-keywords '("kaashif" "pizzaroll"))
+
+(defun erc-cmd-AYY (lmao)
+  (interactive "Payy: ")
+  (erc-send-message (format "ayy %s" lmao)))
