3e06575d296febaf965ab248f46da0f78fb3ec0b
kaashif <kaashif@kaashif.co.uk>
2016-05-14 02:03:53 +0000

    fixed servers?

--- emacs/.ercrc.el
+++ emacs/.ercrc.el
@@ -1,13 +1,15 @@
 (require 'erc)
-(setq erc-server "irc.freenode.net"
+(setq erc-server "irc.nixers.net"
 	  erc-port 6667
 	  erc-prompt-for-password nil
 	  erc-autojoin-channels-alist '(("freenode.net" "#ayylmao1234")
-									("nixers.net" "#unix"))
+									("nixers.net" "#unix")
+									("unix.chat" "#unix"))
 	  erc-nick "kaashif1"
 	  erc-user-full-name "Kaashif Hymabaccus"
 	  erc-insert-timestamp-function 'erc-insert-timestamp-left
-	  erc-nickserv-passwords '(("irc.nixers.net" (("kaashif1" . "megadeth"))))
+	  erc-nickserv-passwords '(("irc.nixers.net" (("kaashif1" . "megadeth")))
+							   ("unix.chat" (("kaashif1" . "megadeth"))))
 	  erc-keywords '("kaashif" "pizzaroll"))
 
 (defun erc-cmd-AYY (lmao)
