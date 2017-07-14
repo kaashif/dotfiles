e48633b5438e4f690e30021ead1d80d2871676d6
Kaashif Hymabaccus <kaashif@kaashif.co.uk>
2017-07-15 00:30:55 +0100

    openbsd updated config for luakit

--- www/.config/luakit/theme.lua
+++ www/.config/luakit/theme.lua
@@ -5,9 +5,9 @@
 local theme = {}
 
 -- Default settings
-theme.font = "Terminus normal 9"
+theme.font = "Terminus normal 12"
 theme.fg   = "#fff"
-theme.bg   = "#101010"
+theme.bg   = "#000"
 
 -- Genaral colours
 theme.success_fg = "#0f0"
@@ -32,6 +32,13 @@ theme.menu_title_bg             = "#fff"
 theme.menu_primary_title_fg     = "#f00"
 theme.menu_secondary_title_fg   = "#666"
 
+theme.menu_disabled_fg = "#999"
+theme.menu_disabled_bg = theme.menu_bg
+theme.menu_enabled_fg = theme.menu_fg
+theme.menu_enabled_bg = theme.menu_bg
+theme.menu_active_fg = "#060"
+theme.menu_active_bg = theme.menu_bg
+
 -- Proxy manager
 theme.proxy_active_menu_fg      = '#000'
 theme.proxy_active_menu_bg      = '#FFF'
@@ -40,30 +47,40 @@ theme.proxy_inactive_menu_bg    = '#FFF'
 
 -- Statusbar specific
 theme.sbar_fg         = "#fff"
-theme.sbar_bg         = "#101010"
+theme.sbar_bg         = "#000"
 
 -- Downloadbar specific
 theme.dbar_fg         = "#fff"
-theme.dbar_bg         = "#101010"
+theme.dbar_bg         = "#000"
 theme.dbar_error_fg   = "#F00"
 
 -- Input bar specific
 theme.ibar_fg           = "#000"
-theme.ibar_bg           = "#fff"
+theme.ibar_bg           = "rgba(0,0,0,0)"
 
 -- Tab label
-theme.tab_fg            = "#999"
-theme.tab_bg            = "#101010"
-theme.tab_ntheme        = "#666"
-theme.selected_fg       = "#999"
+theme.tab_fg            = "#888"
+theme.tab_bg            = "#222"
+theme.tab_hover_bg      = "#292929"
+theme.tab_ntheme        = "#ddd"
+theme.selected_fg       = "#fff"
 theme.selected_bg       = "#000"
-theme.selected_ntheme   = "#666"
+theme.selected_ntheme   = "#ddd"
 theme.loading_fg        = "#33AADD"
 theme.loading_bg        = "#000"
+
+theme.selected_private_tab_bg = "#3d295b"
+theme.private_tab_bg    = "#22254a"
 
 -- Trusted/untrusted ssl colours
 theme.trust_fg          = "#0F0"
 theme.notrust_fg        = "#F00"
 
+-- General colour pairings
+theme.ok = { fg = "#000", bg = "#FFF" }
+theme.warn = { fg = "#F00", bg = "#FFF" }
+theme.error = { fg = "#FFF", bg = "#F00" }
+
 return theme
+
 -- vim: et:sw=4:ts=8:sts=4:tw=80
