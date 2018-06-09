d6bb25da96e692db7e70d4c6a1c9dbae73bf0f0b
Kaashif Hymabaccus <kaashif@kaashif.co.uk>
2018-06-09 22:46:45 +0100

    dbus-launch firefox

--- dwm/config.h
+++ dwm/config.h
@@ -56,7 +56,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
 static const char *termcmd[]  = { "xfce4-terminal", NULL };
-static const char *browsercmd[]  = { "firefox", NULL };
+static const char *browsercmd[]  = { "dbus-launch", "firefox", NULL };
 static const char *editorcmd[]  = { "emacs", NULL };
 
 static Key keys[] = {
